f5d3492f37fd9fc9f3ba236af64a8e8ab3e340cf
Dashtiss <brandontisserand@outlook.com>
2023-09-10 13:25:23 -0500

    Message

--- AntiSurfaceCamping.cs
+++ AntiSurfaceCamping.cs
@@ -1,13 +1,25 @@
 ﻿namespace AntiSurfaceCamping
 {
-    using Exiled.API.Features;
-    using Exiled.API.Interfaces;
     using System;
+
+    using CommandSystem;
+
+    using Exiled.API.Enums;
+    using Exiled.API.Extensions;
+    using Exiled.API.Features;
+    using Exiled.CustomItems.API.Features;
+    using Exiled.Events.Handlers;
+    using Exiled.Permissions.Extensions;
+    using PlayerRoles;
+    using PluginAPI.Core;
+    using Log = PluginAPI.Core.Log;
+
     public class Plugin : Plugin<Config>
     {
+        
         public override string Name => "AntiSurfaceCamping";
         public override string Author => "Dashtiss";
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(1, 0, 1);
         public override void OnEnabled()
         {
             Log.Info("My plugin has been enabled!");
@@ -15,6 +27,13 @@
         public override void OnDisabled()
         {
             Log.Info("My plugin has been disabled!");
+        }
+        private void SurfaceCheck()
+        {
+            if (PluginAPI.Core.Round.IsRoundStarted)
+            {
+                Log.Info('RoundStarted')
+            }
         }
     }
 }
