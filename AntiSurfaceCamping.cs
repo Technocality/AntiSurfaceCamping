fe22fec02a0871f330646daee59a2173ee267afa
Dashtiss <brandontisserand@outlook.com>
2023-09-09 23:16:39 -0500

    Add project files.

---
+++ AntiSurfaceCamping.cs
@@ -0,0 +1,20 @@
+﻿namespace AntiSurfaceCamping
+{
+    using Exiled.API.Features;
+    using Exiled.API.Interfaces;
+    using System;
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "AntiSurfaceCamping";
+        public override string Author => "Dashtiss";
+        public override Version Version => new Version(1, 0, 0);
+        public override void OnEnabled()
+        {
+            Log.Info("My plugin has been enabled!");
+        }
+        public override void OnDisabled()
+        {
+            Log.Info("My plugin has been disabled!");
+        }
+    }
+}
---
+++ Config.cs
@@ -0,0 +1,16 @@
+﻿namespace AntiSurfaceCamping
+{
+    using System.ComponentModel;
+    using Exiled.API.Interfaces;
+    public class Config : IConfig
+    {
+        [Description("Whether or not the plugin is enabled.")]
+        public bool IsEnabled { get; set; }
+        [Description("Debug.")]
+        public bool Debug { get; set; }
+        [Description("What is broadcasted to them if someone is on surface.")]
+        public string BraoadcastText { get; set; } = "You are slowly taking radiation poisoning";
+        [Description("How Much damage players take on surface")]
+        public int Damage { get; set; } = 5; // Set to 5 by default.
+    }
+}
