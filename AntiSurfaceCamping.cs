2a38ee913d97f4d5b07f281b5cdf34acd77144ed
Dashtiss <brandontisserand@outlook.com>
2023-09-10 19:56:23 -0500

    ##Updated shit

--- AntiSurfaceCamping.cs
+++ AntiSurfaceCamping.cs
@@ -1,39 +1,47 @@
-﻿namespace AntiSurfaceCamping
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+namespace AntiSurfaceCamping
 {
-    using System;
-
-    using CommandSystem;
-
-    using Exiled.API.Enums;
-    using Exiled.API.Extensions;
-    using Exiled.API.Features;
-    using Exiled.CustomItems.API.Features;
-    using Exiled.Events.Handlers;
-    using Exiled.Permissions.Extensions;
-    using PlayerRoles;
-    using PluginAPI.Core;
-    using Log = PluginAPI.Core.Log;
 
     public class Plugin : Plugin<Config>
     {
-        
+        private static readonly Dictionary<Player, TimeSpan> PlayerSurfaceTimes = new Dictionary<Exiled.API.Features.Player, TimeSpan>();
+        private static readonly TimeSpan UpdateInterval = TimeSpan.FromSeconds(1);
+
+
         public override string Name => "AntiSurfaceCamping";
         public override string Author => "Dashtiss";
-        public override Version Version => new Version(1, 0, 1);
-        public override void OnEnabled()
-        {
-            Log.Info("My plugin has been enabled!");
-        }
+        public override Version Version => new Version(1, 0, 3);
         public override void OnDisabled()
         {
-            Log.Info("My plugin has been disabled!");
+            Log.Info("My AntiSurfaceCamping is disabled!");
         }
-        private void SurfaceCheck()
+        public void OnUpdate()
         {
-            if (PluginAPI.Core.Round.IsRoundStarted)
+            // Update player surface times at the specified interval.
+            foreach (Player player in Player.List)
             {
-                Log.Info('RoundStarted')
+                if (player.Zone == ZoneType.Surface)
+                {
+                    if (!PlayerSurfaceTimes.ContainsKey(player))
+                    {
+                        PlayerSurfaceTimes[player] = TimeSpan.Zero;
+                    }
+                    PlayerSurfaceTimes[player] += UpdateInterval;
+                    if (PlayerSurfaceTimes[player].TotalSeconds >= Config.MaxSurfaceTime)
+                    {
+                        // Apply damage to the player.
+                        ApplyDamageToPlayer(player);
+                    }
+                }
             }
+        }
+        private void ApplyDamageToPlayer(Player player)
+        {
+            // You can customize the damage amount in your configuration.
+            int damageAmount = Config.Damage;
+            // Apply damage to the player.
+            player.Hurt(damageAmount);
         }
     }
 }
